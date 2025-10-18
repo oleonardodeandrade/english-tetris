5d2912a1d9a396b91e5d408cee99a5204ac3e88e
Leonardo Andrade <leonardoh.deandrade@gmail.com>
2025-10-18 18:46:38 -0300

    feat: define tetromino shapes and colors

---
+++ src/data/tetrominoes.ts
@@ -0,0 +1,74 @@
+import { Tetromino, TetrominoType } from '../types/game.types';
+
+export const TETROMINOES: Record<TetrominoType, Tetromino> = {
+  I: {
+    type: 'I',
+    shape: [
+      [0, 0, 0, 0],
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+    color: '#06b6d4',
+  },
+  O: {
+    type: 'O',
+    shape: [
+      [1, 1],
+      [1, 1],
+    ],
+    color: '#eab308',
+  },
+  T: {
+    type: 'T',
+    shape: [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ],
+    color: '#a855f7',
+  },
+  S: {
+    type: 'S',
+    shape: [
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0],
+    ],
+    color: '#22c55e',
+  },
+  Z: {
+    type: 'Z',
+    shape: [
+      [1, 1, 0],
+      [0, 1, 1],
+      [0, 0, 0],
+    ],
+    color: '#ef4444',
+  },
+  J: {
+    type: 'J',
+    shape: [
+      [1, 0, 0],
+      [1, 1, 1],
+      [0, 0, 0],
+    ],
+    color: '#3b82f6',
+  },
+  L: {
+    type: 'L',
+    shape: [
+      [0, 0, 1],
+      [1, 1, 1],
+      [0, 0, 0],
+    ],
+    color: '#f97316',
+  },
+};
+
+export const TETROMINO_TYPES: TetrominoType[] = ['I', 'O', 'T', 'S', 'Z', 'J', 'L'];
+
+export const getRandomTetromino = (): Tetromino => {
+  const randomType = TETROMINO_TYPES[Math.floor(Math.random() * TETROMINO_TYPES.length)];
+  return TETROMINOES[randomType];
+};
